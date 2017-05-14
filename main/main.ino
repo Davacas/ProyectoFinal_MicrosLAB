a181fe1ac8a75bf04bc0fc762478258ea972a76b
AlanCastilloMontes <a.c.m.e.productions@hotmail.com>
2017-05-13 21:39:46 -0500

    Con función para modificar rango de frecuencias [usuario]

--- main/main.ino
+++ main/main.ino
@@ -15,7 +15,7 @@ void setup() {
   pinMode(pin_bocina_l, OUTPUT);
   pinMode(pin_bocina_r, OUTPUT);
   Serial.begin(9600);
-  Serial.println("\t\tINSTRUMETO THEREMÍN");
+  Serial.println("\t\tINSTRUMENTO THEREMÍN");
   Serial.println("Hecho por Alan Castillo Montes y Daniel Vargas Castro");
   Serial.println("como proyecto final para el laboratorio de microcomputadoras.");
   Serial.println("Facultad de Ingeniería, UNAM. Semestre 2017-2.");
@@ -37,6 +37,9 @@ void loop() {
         Serial.println("Bocinas encendidas.");
         break;
       case '2':
+        cambiar_tonos();
+        break;
+      case '3':
         instrucciones();
         break;
       default:
@@ -47,10 +50,10 @@ void loop() {
     tono_l = map (analogRead(pin_resistencia_l), 0, 1023, min_f_l, max_f_l);
     tono_r = map (analogRead(pin_resistencia_r), 0, 1023, min_f_r, max_f_r);
     tone(pin_bocina_l, tono_l);
-    delay(5);
+    delay(8);
     noTone(pin_bocina_l);
     tone(pin_bocina_r, tono_r);
-    delay(5);
+    delay(8);
     noTone(pin_bocina_r);
   }
 
@@ -61,7 +64,41 @@ void instrucciones() {
   Serial.println("Teclea el número que corresponda a la acción que deseas realizar:");
   Serial.println("\t0: para apagar las bocinas.");
   Serial.println("\t1: para prender las bocinas.");
-  Serial.println("\t2: para mostrar de nuevo las instrucciones.");
+  Serial.println("\t2: para establecer el rango de tonos.");
+  Serial.println("\t3: para mostrar de nuevo las instrucciones.");
   Serial.println("Utiliza las perillas para controlar el volumen de cada bocina.");
+}
+
+void cambiar_tonos() {
+  Serial.println("NOTA: Consulta el archivo 'notas.h' para ver cada nota y su frecuencia.");
+  Serial.println("NOTA: Escribe ',' al final de cada valor ingresado.");
+  Serial.println("Configurando bocina izquierda...");
+  Serial.print("\tIngresa la frecuencia más baja: ");
+  min_f_l = leerTono();
+  Serial.print("\tIngresa la frecuencia más alta: ");
+  max_f_l = leerTono();
+  Serial.println("Configurando bocina derecha...");
+  Serial.print("\tIngresa la frecuencia más baja: ");
+  min_f_r = leerTono();
+  Serial.print("\tIngresa la frecuencia más alta: ");
+  max_f_r = leerTono();
+  instrucciones();
+}
+
+int leerTono(){
+  int charCatcher=0;
+  String inputStr="";
+  while (charCatcher != ',') {
+    if (Serial.available() > 0) {
+      charCatcher = Serial.read();
+      inputStr += (char)charCatcher;
+    }
+  }
+  inputStr.remove(inputStr.length()-1);
+  charCatcher = (int)round(inputStr.toFloat());
+  Serial.println(charCatcher);
+  inputStr = "";
+  
+  return charCatcher;
 }
 
